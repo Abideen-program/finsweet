2558778c1ebe80e0de579e16a9f36aecea9f5804
Abideen Olafimihan <olafimihana@gmail.com>
2023-08-09 21:08:50 +0100

    added the logo component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,27 @@
+const Logo = () => {
+  return (
+    <div className="border border-red-600 px-5 md:px-10 py-3 my-3 flex justify-between items-center gap-3">
+      <div className="w-[100px]">
+        <img src="./images/Logo1.png" alt="" />
+      </div>
+
+      <div className="w-[100px]">
+        <img src="./images/Logo2.png" alt="" />
+      </div>
+
+      <div className="w-[100px]">
+        <img src="./images/Logo3.png" alt="" />
+      </div>
+
+      <div className="w-[100px]">
+        <img src="./images/Logo4.png" alt="" />
+      </div>
+
+      <div className="w-[100px]">
+        <img src="./images/Logo5.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Logo;
